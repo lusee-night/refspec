78bba4c10a3e337f41e6cf024614253bfb4b388b
buddhasystem <potekhin@bnl.gov>
2022-03-24 12:18:08 -0400

    Updated the core to utilize CLI parsing

--- test/test_response.cpp
+++ test/test_response.cpp
@@ -9,29 +9,79 @@
 #include <sstream>
 
 
+// CLI aruments handling:
+#include "lyra.hpp"
+
 int main(int argc, char *argv[]) {
 
-  if (argc!=3) {
-    std::cout << "Specify taps_start taps_end at the command line." <<std::endl;
-    return 1;
+
+  bool verbose        =   false;
+  bool cimode         =   false;
+
+  size_t taps_start   = 0;
+  size_t taps_end     = 0;
+
+  size_t notch_start  = 0;
+  size_t notch_end    = 1;
+
+  size_t win_start    = 0;
+  size_t win_end      = 4;
+
+  auto cli = lyra::cli()
+      | lyra::opt(verbose)
+        ["-v"]["--verbose"]("verbose" )
+      | lyra::opt(cimode)
+        ["-c"]["--cimode"]("cimode" )        
+      | lyra::opt(taps_start, "taps_start" )
+        ["-s"]["--taps_start"]("taps_start")
+      | lyra::opt(taps_end,   "taps_end" )
+        ["-e"]["--taps_end"]("taps_end")
+      | lyra::opt(notch_start, "notch_start" )
+        ["-d"]["--notch_start"]("notch_start")
+      | lyra::opt(notch_end,   "notch_end" )
+        ["-r"]["--notch_end"]("notch_end")
+      | lyra::opt(win_start, "win_start" )
+        ["-f"]["--win_start"]("win_start")
+      | lyra::opt(win_end,   "win_end" )
+        ["-t"]["--win_end"]("win_end")
+     ;
+
+    auto result = cli.parse( { argc, argv } );
+    if ( !result ) {
+            std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+            exit(1);
+    }
+
+  if(verbose) {
+    std::cout << "*** Verbose mode activated ***" << std::endl;
   }
 
-  int taps_start = atoi(argv[1]);
-  int taps_end = atoi(argv[2]);
-  size_t notch_start = 0;
-  size_t notch_end = 1;
-  size_t win_start = 0;
-  size_t win_end = 4;
-  bool CI_mode = false; 
-  
-  if (taps_start <0) {
-    std::cout << "CI test" <<std::endl;
-    taps_start = taps_end = 4;
+  if(cimode) {
+    taps_start  = taps_end  = 4;
     notch_start = notch_end = 0;
-    win_start = win_end = 3;
-    CI_mode = true;
+    win_start   = win_end   = 3;
   }
-  
+
+  if(verbose) {
+    if(cimode) {
+      std::cout << "*** Switching to CI mode ***" << std::endl;
+    }
+    std::cout << "Taps start:  " << taps_start  <<"       Taps end:"  << taps_end   << std::endl;
+    std::cout << "Notch start: " << notch_start <<"       Notch end:" << notch_end  << std::endl;
+    std::cout << "Win start:   " << win_start   <<"       Win end:"   << win_end    << std::endl;    
+  }
+
+  if(taps_start==0 || taps_end==0) {
+    if(verbose) {
+      std::cout << "*** Error: Specify taps_start and taps_end at the command line. ***" << std::endl;
+    }
+    exit(-1);
+  }
+
+
+
+  // Finished parsing, starting setup:
+
   SpecConfig cfg;
 
   cfg.Ntaps       = 7;
@@ -47,26 +97,32 @@ int main(int argc, char *argv[]) {
   double central  = 10; // doesn't really matter
 
   for (size_t Ntaps=taps_start; Ntaps<=taps_end; Ntaps++) {
-    std::cout << "Doing taps: " << Ntaps << std::endl;
-    for (int notch = notch_start; notch <= notch_end; notch++) {
-       for (int win = win_start; win <= win_end; win++) {
+
+    if(verbose) {
+      std::cout << "Doing taps: " << Ntaps << std::endl;
+    }
+    
+    for (int notch=notch_start; notch <= notch_end; notch++) {
+       for (int win=win_start; win <= win_end; win++) {
         std::stringstream fname;
-	if (CI_mode) 
-	  fname << "response_test.dat";
-	else
-	  fname << "response_" << Ntaps<< "_" << notch << "_" << win << ".dat";
-        std::ofstream outfile;
-        outfile.open(fname.str());
-        cfg.Ntaps = Ntaps;
-        cfg.notch = notch;
-	cfg.window = window_t(win);
-        for (double freq = central -3 ; freq< central + 3; freq+=0.01) {
-	  SignalGenerator signal(cfg.Nfft, cfg.Nchannels, blocks, freq*fundamental, cfg.sampling_rate, Ampl, noiseA);
-	  RefSpectrometer S(&signal,&cfg);    
-          SpecOutput O(&cfg);
-	        S.run(&O);
-	        outfile <<freq-central << " " << O.avg_pspec[0][int(central)] << std::endl;
-        }
+	      if (cimode) 
+      	  fname << "response_test.dat";
+	      else
+	        fname << "response_" << Ntaps<< "_" << notch << "_" << win << ".dat";
+          std::ofstream outfile;
+          outfile.open(fname.str());
+          cfg.Ntaps = Ntaps;
+          cfg.notch = notch;
+	        cfg.window = window_t(win);
+
+          for (double freq=central-3 ; freq<central+3; freq+=0.01) {
+	          SignalGenerator signal(cfg.Nfft, cfg.Nchannels, blocks, freq*fundamental, cfg.sampling_rate, Ampl, noiseA);
+	          RefSpectrometer S(&signal,&cfg);    
+            SpecOutput O(&cfg);
+	          S.run(&O);
+	          outfile <<freq-central << " " << O.avg_pspec[0][int(central)] << std::endl;
+          }
+
         outfile.close();
       } 
     } 
