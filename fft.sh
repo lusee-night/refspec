ec8d4a6cb0bcd102916f73fa3b0958eebf1d7f90
buddhasystem <potekhin@bnl.gov>
2022-12-21 21:38:09 -0500

    Attemting to build FFTW in the CI, because the default image doesn't appear to have .so

---
+++ fft.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget http://www.fftw.org/fftw-3.3.10.tar.gz
+tar xfvz fftw-3.3.10.tar.gz
+cd fftw-3.3.10
+
+# Build two versions of the library, with and w/o the float option:
+
+./configure --enable-float --enable-shared
+make -j4 # if you have at least 4 cores that helps speed
+make install
+
+./configure --enable-shared
+make -j4 # if you have at least 4 cores that helps speed
+make install
